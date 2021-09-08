8d45e23dd95fbb599f3a8a0ed98033ae4252cbda
Kevin Stamenkovski <kevin.stamenkovski@gmail.com>
2021-09-08 21:14:18 +0200

    Adding project to github

---
+++ Game/index.js
@@ -0,0 +1,8 @@
+var playerHP = 100 ;
+var enemyHP = 100 ;
+    document.getElementById("playerHealth").innerHTML = playerHP;
+    document.getElementById("enemyHealth").innerHTML = enemyHP;
+
+    while (playerHP > 0 || enemyHP > 0) {
+        
+    }
---
+++ app/ts/app.component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>Select Your Gender</h1>'
+})
+
+export class AppComponent {}
